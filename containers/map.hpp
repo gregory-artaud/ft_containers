d2a1ba7c884add4fbfabccef1d198258a286cf5c
Grégory Artaud <gregory.rtd@gmail.com>
2022-05-01 00:47:52 +0200

    advanced on map roadmap

--- containers/map.hpp
+++ containers/map.hpp
@@ -12,8 +12,11 @@
 **
 ** TODO:
 **
-** - Finish roadmap
+** - Recheck roadmap
 ** - Implement everything
+** - value_comp and value_compare
+** - relational operators
+** - iterators typedefs
 **
 */
 
@@ -25,13 +28,13 @@ namespace ft {
 		typedef T mapped_type;
 		typedef std::pair<const key_type, mapped_type> value_type;
 		typedef Compare key_compare;
-		// typedef [...] value_compare;
 		typedef Alloc allocator_type;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
 		typedef typename allocator_type::reference reference;
 		typedef typename allocator_type::const_reference const_reference;
 		typedef size_t size_type;
+        // typedef [...] value_compare;
 		
 		// iterators and const_iterators
 		// TODO
@@ -41,39 +44,88 @@ namespace ft {
 		
 		public:
 			// Constructors and Destructor
-			map (); // TODO
-			~map (); // TODO
+			map(const key_compare& comp = key_compare(),
+                const allocator_type& alloc = allocator_type()); // TODO
+
+            template <class InputIterator>
+            map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(),
+                    const allocator_type& alloc = allocator_type()); // TODO
+            
+            map(const map& x); // TODO
+
+			~map(); // TODO
 
 			/*
 			** Iterators
 			*/
+            iterator begin(); // TODO
+            const_iterator begin() const; // TODO
+            iterator end(); // TODO
+            const_iterator end() const; // TODO
+            reverse_iterator rbegin(); // TODO
+            const_reverse_iterator rbegin() const; // TODO
+            reverse_iterator rend(); // TODO
+            const_reverse_iterator rend() const; // TODO
 
 			/*
 			** Capacity
 			*/
+            bool empty() const; // TODO
+            size_type max_size() const
+            {
+                return allocator_type().max_size(); 
+            }
+            size_type size() const; // TODO
 
 			/*
 			** Element access
 			*/
+            iterator lower_bound(const key_type& k); // TODO
+            const_iterator lower_bound(const key_type& k) const; // TODO
+            iterator upper_bound(const key_type& k); // TODO
+            const_iterator upper_bound(const key_type& k) const; // TODO
+            mapped_type& operator[](const key_type& k); // TODO
 
 			/*
 			** Modifiers
 			*/
+            void erase(iterator position); // TODO
+            size_type erase(const key_type& k); // TODO
+            void erase(iterator first, iterator last); // TODO
+            void clear(); // TODO
+            ft::pair<iterator,bool> insert(const value_type& val); // TODO
+            iterator insert(iterator position, const value_type& val); // TODO
+            template <class InputIterator>
+            void insert(InputIterator first, InputIterator last); // TODO
 
 			/*
 			** Observers
 			*/
+            size_type count(const key_type& k) const; // TODO
+            ft::pair<const_iterator,const_iterator> equal_range(const key_type& k) const; // TODO
+            ft::pair<iterator,iterator> equal_range(const key_type& k); // TODO
+            iterator find(const key_type& k); // TODO
+            const_iterator find(const key_type& k) const; // TODO
 
 			/*
 			** Operations
 			*/
+            map& operator=(const map& x); // TODO
+            void swap(map& x); // TODO
 
 			/*
-			** Allocator
+			** Allocator and Compare
 			*/
 			// NB: return a copy of the allocator
-			allocator_type get_allocator() const { return allocator_type(_alloc); }
+			allocator_type get_allocator() const
+            {
+                return allocator_type(_alloc);
+            }
+            key_compare key_comp() const; // TODO
 	};
+
+    template <class Key, class T, class Compare, class Alloc>
+    void swap(map<Key,T,Compare,Alloc>& x, map<Key,T,Compare,Alloc>& y); // TODO
 }
 
 #endif
--- includes/data_structure/binary_search_tree.hpp
+++ includes/data_structure/binary_search_tree.hpp
@@ -6,23 +6,92 @@
 namespace ft
 {
     template <typename T>
-    class BinarySearchTree
+    class BST_Node
     {
         public:
             typedef T value_type;
 
-            BinarySearchTree()
+            BST_Node()
             {
-                value = value_type();
-                left = NULL;
-                right = NULL;
+                _value = value_type();
+                _left = NULL;
+                _right = NULL;
+            }
+
+            BST_Node(const BST_Node& nd)
+            {
+                operator=(nd);
+            }
+
+            ~BST_Node() {}
+
+            BST_Node& operator=(const BST_Node& nd)
+            {
+                _value = nd._value;
+                _left = nd._left;
+                _right = nd._right;
+                return *this;
+            }
+
+            BST_Node* const & getLeft() const
+            {
+                return _left;
+            }
+
+            BST_Node* const & getRight() const
+            {
+                return _right;
+            }
+
+            value_type const & getValue() const
+            {
+                return _value;
+            }
+
+            BST_Node& setValue(const value_type& val)
+            {
+                _value = val;
+                return *this;
+            }
+
+            BST_Node& setLeft(const BST_Node*& nd)
+            {
+                _left = nd;
+                return *this;
+            }
+
+            BST_Node& setRight(const BST_Node*& nd)
+            {
+                _right = nd;
+                return *this;
+            }
+
+        private:
+            value_type _value;
+            BST_Node* _left;
+            BST_Node* _right;
+    };
+
+
+    template <typename T, class Compare = ft::less<T>, class Node = BST_Node<T>,
+        class Alloc = std::allocator<T> >
+    class BinarySearchTree
+    {
+        public:
+            typedef T value_type;
+            typedef Alloc allocator_type;
+
+            BinarySearchTree(const allocator_type& allocator = allocator_type())
+            {
+                _root = NULL;
+                _alloc = allocator;
             }
 
             ~BinarySearchTree() {}
+
         private:
-            value_type value;
-            BinarySearchTree* left;
-            BinarySearchTree* right;
+            Node* _root;
+            allocator_type _alloc;
     };
 }
 #endif
